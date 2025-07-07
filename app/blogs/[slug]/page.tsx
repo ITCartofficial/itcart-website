cd95921a8455906302ef11813149e58b5b3da2b2
numandotdev <numandotdev@gmail.com>
2025-07-07 16:38:02 +0530

    refactor: Standardize import statements and improve slug handling in DynamicPage component

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,9 +1,8 @@
-
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
 import "../singlePost.css"; // Ensure this path is correct based on your project structure
-import Image from 'next/image';
-import { GET_POST_BY_SLUG } from '@/lib/wp/queries';
-import client from '@/lib/wp/graphqlClient';
+import Image from "next/image";
+import { GET_POST_BY_SLUG } from "@/lib/wp/queries";
+import client from "@/lib/wp/graphqlClient";
 // import { Post } from '@/types/wordpress';
 
 interface PageProps {
@@ -11,27 +10,38 @@ interface PageProps {
 }
 
 export default async function DynamicPage({ params }: PageProps) {
-  const slugPath = params.slug.join('/');
+  const slugParam = params.slug;
+  // Normalize to array
+  const slugSegments = Array.isArray(slugParam)
+    ? slugParam
+    : slugParam
+    ? [slugParam]
+    : [];
+
+  const slugPath = slugSegments.join("/");
 
   // If no page, try fetching a post by slug
-  const { data: postData }= await client.query({
+  const { data: postData } = await client.query({
     query: GET_POST_BY_SLUG,
     variables: { slug: slugPath },
   });
 
   if (postData.post) {
-    console.log('Post Data:', postData.post);
+    // console.log('Post Data:', postData.post);
     return (
-      <article id='singlePostContainer'>
+      <article id="singlePostContainer">
         <h1>{postData.post.title}</h1>
         <Image
-          src={postData.post.featuredImage.node.sourceUrl || ""}
-          alt={postData.post.title || ""}
+          src={postData?.post?.featuredImage?.node?.sourceUrl || ""}
+          alt={postData?.post?.title || ""}
           width={500}
           height={300}
-          className='w-full h-[300px] object-cover mb-4'
+          className="w-full h-[300px] object-cover mb-4"
         />
-        <div id='postContent' dangerouslySetInnerHTML={{ __html: postData.post.content }} />
+        <div
+          id="postContent"
+          dangerouslySetInnerHTML={{ __html: postData?.post?.content }}
+        />
       </article>
     );
   }
