81c8ffb342913192a86fc3eadf7cc59fbf3611e2
numandotdev <numandotdev@gmail.com>
2025-08-05 10:38:35 +0530

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,43 @@
-import { Montserrat } from "next/font/google"
-import "./globals.css"
-import Header from "@/app/components/layouts/Header"
-import Footer from "@/app/components/layouts/Footer"
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { ReactNode } from "react";
-import { HomeProvider } from "./homeContext";
-
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import Header from "@/app/components/layouts/Header";
+import Footer from "@/app/components/layouts/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
   display: "swap",
   weight: ["400", "500", "600", "700"],
-})
+});
 
 export const metadata = {
   title: "Best AI Automation Company In US | iTCART",
-  description: "iTCart - The best AI automation company in US, offering cutting-edge solutions to streamline workflows, enhance efficiency, and drive business growth.",
-}
+  description:
+    "iTCart - The best AI automation company in US, offering cutting-edge solutions to streamline workflows, enhance efficiency, and drive business growth.",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </head>
-      <body className={`w-full overflow-x-hidden ${montserrat.variable} font-sans antialiased relative`}>
+      <body
+        className={`w-full overflow-x-hidden ${montserrat.variable} font-sans antialiased relative`}
+      >
         <Header />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
         <Analytics />
         <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
-
