8a36d76d46f1191c96dba4bc030303a7205b7e23
abhinavtv-itcart <abhinav.t@itcart.io.com>
2025-09-24 19:07:38 +0530

    build ai section component

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,7 @@ import { notFound } from "next/navigation";
 import client from "@/lib/wp/graphqlClient";
 import { GET_ALL_POST_SLUGS } from "@/lib/wp/queries";
 import { caseStudyData } from '@/lib/data/caseStudyData';
+import AiSection from "./(features)/home/AiSection";
 
 
 
@@ -87,6 +88,15 @@ const HomeTemplate = () => {
           cardDetails={aboutBrief}
         />
       </div>
+
+      <div className="bg-black py-16 container mx-auto lg:px-10">
+        <AiSection
+          title="Our AI-powered Solutions for Intelligent Automation"
+          cardDetails={aboutBrief}
+        />
+
+      </div>
+
       <div className="bg-white">
         {/* <FaqSection faqsData={faqsData} faqsCount={4} title="Our AI-powered Solutions for Intelligent Automation" /> */}
         <PatentPendingSection />
--- components/cards/ImageInfoCard.tsx
+++ components/cards/ImageInfoCard.tsx
@@ -2,15 +2,27 @@ import BodyText from "@/components/typography/BodyText"
 import GradientTitle from "@/components/typography/GradientTitle"
 import { ImageInfoCardProps } from "@/types/PropsTypes"
 import React from "react"
+import OutlineBtn from "../buttons/OutlineBtn"
+import { FaArrowRight } from "react-icons/fa6"
 
 
-
-const ImageInfoCard: React.FC<ImageInfoCardProps> = ({ bgImage, title, description, descriptionSize, className }) => {
+const ImageInfoCard: React.FC<ImageInfoCardProps> = ({ bgImage, title, description, descriptionSize, className, showBtn }) => {
     return (
-        <div style={{ backgroundImage: `url(${bgImage})` }} className={`bg-cover min-h-[100vh] lg:min-h-[480px] bg-center w-full rounded-2xl overflow-hidden group hover:-translate-y-4 transition-all duration-500 ease-in-out ${className && className}`}>
+        <div style={{ backgroundImage: `url(${bgImage})` }} className={`bg-cover min-h-[100vh] lg:min-h-[580px] bg-center w-full rounded-2xl overflow-hidden group hover:-translate-y-4 transition-all duration-500 ease-in-out ${className && className}`}>
             <div className="wrapper flex flex-col gap-4 justify-end p-8 w-full h-full" style={{ background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, #000 85%)' }}>
                 <GradientTitle text={title} className="text-[24px] lg:text-[36px]" theme="dark" />
-                <BodyText text={description} color="#ffffff" fontSize={descriptionSize || "18px"}  />
+                <BodyText text={description} color="#ffffff" fontSize={descriptionSize || "18px"} />
+
+                {
+                    showBtn &&
+                    <OutlineBtn
+                        url='/'
+                        text="Read More"
+                        textColor='#ffffff'
+                        icon={<FaArrowRight className="text-sm font-semibold text-white" />}
+                    />
+                }
+
             </div>
         </div>
     )
