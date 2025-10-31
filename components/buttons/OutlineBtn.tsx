4af980202a474ab9744b027fd42d933a144fc261
abhinavtv-itcart <abhinav.t@itcart.io.com>
2025-10-31 10:31:16 +0530

    qa section 2

--- components/buttons/OutlineBtn.tsx
+++ components/buttons/OutlineBtn.tsx
@@ -17,44 +17,19 @@ const OutlineBtn: React.FC<OutlineBtnProps> = ({ text, icon, textColor, classNam
                 changgeBtn ?
                     <a
                         onMouseLeave={() => setChangeBtn(false)}
+
                         href={url || ""}
-                        // className={`decoration-0  min-w-[160px] py-3 w-max  group w-max anim-outline-btn ${theme} flex items-center justify-center gap-2 cursor-pointer ${className}`} onClick={onClick}
-                        className={`decoration-0 
-                             px-7
-                            py-3  w-max group ${theme} flex items-center justify-center gap-2 cursor-pointer ${className}`}
+                        // onClick={onClick}
+                         className={`decoration-0  min-w-[160px] py-3 w-max  group w-max  ${theme} flex items-center justify-center gap-2 cursor-pointer ${className}`} onClick={onClick}
+            //             className={`decoration-0 py-3 anim-outline-btn
+            //              px-7 group w-max flex items-center justify-center gap-2 cursor-pointer
+            //    rounded transition-all duration-300 ease-in-out
+            //   bg-[#01FFFF] hover:bg-cyan-300 
+            //   border-1 border-white
+            //   ${theme} ${className}`}
 
                         style={{
-                            border: '1px solid transparent',
-                            backgroundImage: 'linear-gradient(black, black), linear-gradient(to right, #FFFFFF 45%, #45C2CC 100%)',
-                            backgroundOrigin: 'border-box',
-                            backgroundClip: 'padding-box, border-box',
-                            borderRadius: '8px'
-                        }}
-                    >
-
-                        <p className="text-sm font-medium" style={{ color: textColor }}>{text}</p>
-                        <div className="group-hover:translate-x-1 transition-all duration-300 ease-in-out">
-                            {icon === "default" ? <FaArrowRight className={`text-sm font-semibold ${iconColor}`} /> : icon}
-                        </div>
-                    </a>
-
-                    :
-
-                    <a
-                        onMouseEnter={() => setChangeBtn(true)}
-                        onMouseLeave={() => setChangeBtn(false)}
-                        href={url || ""}
-                        onClick={onClick}
-                        className={`decoration-0 py-3 
-                         px-7
-                             group w-max flex items-center justify-center gap-2 cursor-pointer
-               rounded transition-all duration-300 ease-in-out
-              bg-[#01FFFF] hover:bg-cyan-300 
-              border-1 border-white
-              ${theme} ${className}`}
-
-                        style={{
-                            border: '1px solid transparent',
+                            border: '.5px solid transparent',
                             backgroundImage: 'linear-gradient(#01FFFF, #01FFFF), linear-gradient(to right, #03536B, #FFFFFF)',
                             backgroundOrigin: 'border-box',
                             backgroundClip: 'padding-box, border-box',
@@ -72,6 +47,33 @@ const OutlineBtn: React.FC<OutlineBtnProps> = ({ text, icon, textColor, classNam
                             {<FaArrowRight style={{ color: 'black' }} className={`text-sm font-semibold`} />}
                         </div>
                     </a>
+
+                    :
+
+                    <a
+                        onMouseEnter={() => setChangeBtn(true)}
+                        onMouseLeave={() => setChangeBtn(false)}
+                        href={url || ""}
+                        className={`decoration-0 anim-outline-btn  min-w-[160px] py-3 w-max  group w-max  ${theme} flex items-center justify-center gap-2 cursor-pointer ${className}`} onClick={onClick}
+                        // className={`decoration-0 
+                        //      px-7
+                        //     py-3  w-max group ${theme} flex items-center justify-center gap-2 cursor-pointer ${className}`}
+
+                        style={{
+                            border: '.5px solid transparent',
+                            backgroundImage: 'linear-gradient(black, black), linear-gradient(to right, #FFFFFF 45%, #45C2CC 100%)',
+                            backgroundOrigin: 'border-box',
+                            borderRadius: '8px'
+                        }}
+                    >
+
+                        <p className="text-sm font-medium" style={{ color: textColor }}>{text}</p>
+                        <div className="group-hover:translate-x-1 transition-all duration-300 ease-in-out">
+                            {icon === "default" ? <FaArrowRight className={`text-sm font-semibold ${iconColor}`} /> : icon}
+                        </div>
+                    </a>
+
+
             }
 
         </>
